1b50587db5a0021dd116777d17a2caacc2e2c20b
ankits0207 <ankit16121@iiitd.ac.in>
2019-04-11 11:55:36 +0530

    Celsius2Fahrenheit

--- CelsiusToFahrenheit.py
+++ CelsiusToFahrenheit.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 celsiusInput = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheitOutput = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-# This layer would have 1 internal variable specified by units and takes 1d array as the input
+# This layer would have 1 internal variable(1 weight and 1 bias) specified by units and takes 1d array as the input
 layer0 = tf.keras.layers.Dense(units=1, input_shape=[1])
 
 # Assembling the defined layers(layer0 in this case) into model
@@ -28,4 +28,13 @@ plt.show()
 
 print(model.predict([100.0]))
 
+print('Actual weights = 1.8 and 32, Predicted weights:')
+print(layer0.get_weights())
+
 print('Done')
+
+# Creating a slightly complex neural network: 3 input features, dense fully connected hidden layer with 2 neurons
+# and 1 output layer with a single unit
+# hiddenLayer = tf.keras.layers.Dense(units=2, input_shape=[3])
+# outputLayer = tf.keras.layers.Dense(units=1)
+# model = tf.keras.Sequential([hiddenLayer, outputLayer])
